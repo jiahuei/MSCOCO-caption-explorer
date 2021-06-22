356065b7c79c74b945728ba468b3e135fa9c6d44
Tan Jia Huei <tanjiahuei@gmail.com>
2021-06-22 17:15:06 +0800

    `explore_json`: make `eval()` safer

--- explore_json.py
+++ explore_json.py
@@ -27,13 +27,18 @@ def main():
     data = load_json(uploaded_file)
     st.header(f"Filename: {uploaded_file.name}")
     st.markdown(f"---")
+    top1, top2 = st.beta_columns([5, 6])
+    with top1:
+        st.info('You can provide an expression for filtering the loaded JSON "`data`" object.')
+    with top2:
+        st.warning('Note: The expression is evaluated using `eval()`. See https://stackoverflow.com/a/34385055')
     expression = st.text_input(
-        label='Expression for filtering JSON "data" object. Example: data["x"]',
+        label='Expression examples: data["x"], data[0]',
         value="data"
     )
     st.markdown(f"---")
     try:
-        data = eval(expression, {"data": data})
+        data = eval(expression, {"data": data, "__builtins__": {}})
     except Exception:
         st.markdown(f"Invalid expression. The full JSON object is:")
         st.json(data)
